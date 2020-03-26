3ca8ce058f93aa2d7048eeb403e52565dc079daa
Azis Naufal <azis.naufal@gmail.com>
2020-03-26 15:15:01 +0700

    بِسْمِ اللّهِ الرَّحْمَنِ الرَّحِيْ

---
+++ src/kelasutama.java
@@ -0,0 +1,17 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author oazisn
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        jutama jutama = new jutama();
+        jutama.setVisible(true);
+    }
+}
